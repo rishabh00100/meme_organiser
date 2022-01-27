5d20fc8d1d92f64a549ad5ca7bf0d2a87c207fdb
rishabh00100 <rishabhgarodia@gmail.com>
2022-01-26 19:16:00 -0500

    updates for publishing

--- test_meme_classifier.py
+++ test_meme_classifier.py
@@ -1,7 +1,7 @@
 """
 	Script to train a classification model to classify an image as:
-		- Meme
-		- Non Meme
+		- yes_meme
+		- no_meme
 
 	Code source: 
 	https://towardsdatascience.com/how-to-train-an-image-classifier-in-pytorch-and-use-it-to-perform-basic-inference-on-single-images-99465a1e9bf5
@@ -9,12 +9,9 @@
 	This code was modified to run on Google Colab
 """
 
-from google.colab import drive
-drive.mount('/content/drive/')
-
-%matplotlib inline
-%config InlineBackend.figure_format = 'retina'
+import os, sys
 import matplotlib.pyplot as plt
+import argparse
 
 import numpy as np
 import torch
@@ -23,7 +20,15 @@ from torch import optim
 import torch.nn.functional as F
 from torchvision import datasets, transforms, models
 from torch.autograd import Variable
+import warnings
+import time
+warnings.filterwarnings("ignore")
 
+parser = argparse.ArgumentParser(description='Inference script for meme classification')
+parser.add_argument('--path', default='meme_classifier_openSource.pth',
+                    help='name of the model .pth file. Ex: model1.pth')
+
+args = parser.parse_args()
 
 def predict_image(image):
     image_tensor = test_transforms(image).float()
@@ -37,6 +42,8 @@ def predict_image(image):
 def get_random_images(num):
     data = datasets.ImageFolder(data_dir, transform=test_transforms)
     classes = data.classes
+    # print("data.classes", data.classes)
+    # print("data.class_to_idx", data.class_to_idx)
     indices = list(range(len(data)))
     np.random.shuffle(indices)
     idx = indices[:num]
@@ -48,28 +55,58 @@ def get_random_images(num):
     images, labels = dataiter.next()
     return images, labels, classes
 
-
-data_dir = '/content/drive/My Drive/test_dataset/'
+cwd = os.path.abspath(os.getcwd())
+data_dir = os.path.join(cwd, "resource/test_imgs")
 
 # Load model in eval mode for testing
 test_transforms = transforms.Compose([transforms.Resize((224, 224)),
                                       transforms.ToTensor(),
                                      ])
+print("[*] Loading Model")
+start_time = time.time()
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model=torch.load('/content/drive/My Drive/meme_classifier.pth')
+try:
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+        model=torch.load(os.path.join(cwd, args.path))
+    else:
+        device = torch.device("cpu")
+        model=torch.load(os.path.join(cwd, args.path), map_location='cpu')
+except Exception as e:
+    print(e)
+    link = "https://drive.google.com/file/d/1oU1E5LyGQVKDZzgueycjdE15W6kaVxet/view"
+    print("Model not found. Please download the model from {} and save it in the $ROOT as meme_classifier_openSource.pth".format(link))
+    sys.exit()
+
+model.to(device)
 model.eval()
+print("[*] Model successfully loaded in {:.2f} secs".format(time.time() - start_time))
 
 # Run model predictions on random images
+num_imgs = 25
 to_pil = transforms.ToPILImage()
-images, labels, classes = get_random_images(5)
+images, labels, classes = get_random_images(num_imgs)
 fig=plt.figure(figsize=(20,20))
-for ii in range(len(images)):
-    image = to_pil(images[ii])
-    index = predict_image(image)
-    sub = fig.add_subplot(1, len(images), ii+1)
-    res = int(labels[ii]) == index
-    sub.set_title(str(classes[index]) + ":" + str(res))
-    plt.axis('off')
-    plt.imshow(image)
-plt.show()
+counter = 0
+time_pred = 0
+while counter < num_imgs:
+    row_num = 1
+    for ii in range(5):
+        image = to_pil(images[counter])
+        pred_start_time = time.time()
+        index = predict_image(image)
+        pred_time = time.time() - pred_start_time
+        time_pred += pred_time
+        sub = fig.add_subplot(5, 5, counter+1)
+        res = int(labels[counter]) == index
+        sub.set_title("Annotation: " + str(classes[index]) + "\nPredicted correctly:" + str(res))
+        plt.axis('off')
+        plt.imshow(image)
+        counter += 1
+    row_num += 1
+
+print("[*] Average time of prediction: {:.2f} secs / image".format(time_pred/25))
+path = os.path.join(cwd, 'output.png')
+fig.savefig(os.path.join(cwd, 'output.png'), bbox_inches='tight')
+plt.close(fig)
+print("[*] Output saved at: {}".format(path))
